0ce5cddff8b5b84a738216ba0dac8a923390e207
Alex Taxiera <alex.taxiera@gmail.com>
2018-05-09 11:48:33 -0400

    use proper names

--- commands/status.js
+++ commands/status.js
@@ -8,20 +8,23 @@ module.exports = new Command({
   run: async ({ msg, params, bot }) => {
     const option = params.splice(0, 1)[0]
     const fullParam = params.join(' ')
-    const games = await bot.dbm.getGames() || []
-    const names = games.map((val) => val.name)
+    const statuses = await bot.dbm.getStatuses() || []
+    const names = statuses.map((val) => val.name)
 
     switch (option) {
       case 'view':
         return 'Current random statuses:\n' + names.join(',\n')
       case 'add':
-        if (games.includes(fullParam)) return `Statuses already includes "${fullParam}"`
-        bot.dbm.addGame(fullParam)
+      // TODO: maybe supply type with name? maybe too complicated
+        if (statuses.includes(fullParam)) return `Statuses already includes "${fullParam}"`
+        bot.dbm.addStatus(fullParam)
         return `${fullParam} added!`
       case 'del':
-        if (!games.includes(fullParam)) return `Statuses does not include "${fullParam}"`
-        bot.dbm.delGame(fullParam)
+        if (!statuses.includes(fullParam)) return `Statuses does not include "${fullParam}"`
+        // TODO MAYBE YOU SHOULD ADD THIS YOU FUCK
+        bot.dbm.delStatus(fullParam)
         return `${fullParam} deleted!`
+      // TODO: edit option, change name or type
       default:
         return 'Use "add" to add statuses and "del" to delete them!'
     }
