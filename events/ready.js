e68ac8a43275a0be8302001eace683d519dfd0a2
Alex Taxiera <alex.taxiera@gmail.com>
2018-05-16 16:26:23 -0400

    log online as success

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (bot) => {
   await bot.dbm.initialize(bot.guilds)
-  bot.logger.log('online')
+  bot.logger.success('online')
   if (bot.toggles.get('rotateStatus').value) bot.status.startRotate(bot)
   bot.status.default(bot)
 }
