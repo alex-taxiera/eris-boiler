df4f04740da25dabb438c6b1ebe5a5420b6654ef
Alex Taxiera <alex.taxiera@gmail.com>
2018-05-16 16:27:04 -0400

    use setting constructor properly

--- settings/prefix.js
+++ settings/prefix.js
@@ -1,11 +1,14 @@
 const Setting = require('../classes/Setting.js')
 
 module.exports = (bot) => {
-  return new Setting({
-    code: 'prefix',
-    name: 'Default Prefix',
-    onChange: function (bot, value) {
-      bot.dbm.updateDefaultPrefix(value)
+  return new Setting(
+    bot,
+    {
+      code: 'prefix',
+      name: 'Default Prefix',
+      onChange: function (bot, value) {
+        bot.dbm.updateDefaultPrefix(value)
+      }
     }
-  }, bot)
+  )
 }
