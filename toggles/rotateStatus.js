c2539b97100dc7ff2e78184ad6c4111b1b80c1d2
Alex Taxiera <alex.taxiera@gmail.com>
2018-05-16 16:27:04 -0400

    use setting constructor properly

--- toggles/rotateStatus.js
+++ toggles/rotateStatus.js
@@ -1,13 +1,16 @@
 const Toggle = require('../classes/Toggle.js')
 
 module.exports = (bot) => {
-  return new Toggle({
-    code: 'rotateStatus',
-    name: 'Rotating Status',
-    onChange: function (bot, value) {
-      if (value) return bot.status.startRotate()
-      bot.status.endRotate()
-      bot.status.setGame(bot, value)
+  return new Toggle(
+    bot,
+    {
+      code: 'rotateStatus',
+      name: 'Rotating Status',
+      onChange: function (bot, value) {
+        if (value) return bot.status.startRotate()
+        bot.status.endRotate()
+        bot.status.setGame(bot, value)
+      }
     }
-  }, bot)
+  )
 }
