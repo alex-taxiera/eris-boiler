9e897a45622384e6855155b6f9dbd3e67f5b3887
Alex Taxiera <alex.taxiera@gmail.com>
2018-06-08 22:07:15 -0400

    update formatting and dbm calls

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,7 +1,7 @@
 module.exports = async (bot, msg) => {
   if (!msg.member || msg.member.id === bot.user.id) return
 
-  const { prefix } = await bot.dbm.getClient(msg.channel.guild.id)
+  const { prefix } = await bot.dbm.getSettings(msg.channel.guild.id)
   if (!msg.content.startsWith(prefix)) return
 
   const params = msg.content.substring(prefix.length).split(' ')
@@ -11,22 +11,21 @@ module.exports = async (bot, msg) => {
 
   if (params.length < command.parameters.length) {
     return msg.channel.createMessage(msg.author.mention + ' insufficient parameters!')
-    .then((m) => setTimeout(() => m.delete(), 15000))
+      .then((m) => setTimeout(() => m.delete(), 15000))
   }
 
   const perm = bot.permissions.get(command.permission)
   if (!await allow(bot, perm, msg)) {
     return msg.channel.createMessage(msg.author.mention + ' ' + perm.deny())
-    .then((m) => setTimeout(() => m.delete(), 25000))
+      .then((m) => setTimeout(() => m.delete(), 25000))
   }
-  command.run({ params, bot, msg })
-  .then((response) => {
+  command.run({ params, bot, msg }).then((response) => {
     if (command.deleteInvoking) msg.delete().catch((e) => bot.logger.warn('cannot delete messages'))
     if (!response) return
     const content = parseResponse(response)
     return msg.channel.createMessage(content)
-    .then((m) => { if (command.delay > 0) setTimeout(() => m.delete(), command.delay) })
-    .catch(console.error)
+      .then((m) => { if (command.delay > 0) setTimeout(() => m.delete(), command.delay) })
+      .catch(console.error)
   })
 }
 
