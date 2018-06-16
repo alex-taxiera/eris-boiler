de28a269d2ee119d7f7f0392a53de99ba85be40c
Alex Taxiera <alex.taxiera@gmail.com>
2018-06-16 01:37:20 -0400

    remove legacy deny functions

--- src/permissions/admin.js
+++ src/permissions/admin.js
@@ -3,6 +3,5 @@ const Permission = require('../classes/Permission.js')
 module.exports = new Permission({
   name: 'Admin',
   level: 100,
-  check: async (member, bot) => member.id === (await bot.getOAuthApplication()).owner.id,
-  deny: () => 'Must be bot owner!'
+  check: async (member, bot) => member.id === (await bot.getOAuthApplication()).owner.id
 })
--- src/permissions/owner.js
+++ src/permissions/owner.js
@@ -3,6 +3,5 @@ const Permission = require('../classes/Permission.js')
 module.exports = new Permission({
   name: 'Guild Owner',
   level: 80,
-  check: async (member) => member.id === member.guild.ownerID,
-  deny: () => 'Must be guild owner!'
+  check: async (member) => member.id === member.guild.ownerID
 })
