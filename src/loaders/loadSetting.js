a4b207e74a602b396dbf6d0c9906942a70ba751b
Alex Taxiera <alex.taxiera@gmail.com>
2018-06-16 23:01:51 -0400

    remove extra logic from index

--- src/loaders/loadSetting.js
+++
@@ -1,9 +0,0 @@
-module.exports = (bot, settingFile) => {
-  try {
-    const settingName = settingFile.split('.')[0]
-    bot.settings.set(settingName, require(`../settings/${settingFile}`)(bot))
-    // bot.logger.success(`Loaded ${bot.settings.get(settingName).name} Setting`)
-  } catch (e) {
-    bot.logger.error(`Unable to load setting ${settingFile}: ${e}`)
-  }
-}
