5d0832a540cdfa10ff33b06505556845f18e4c33
Alex Taxiera <alex.taxiera@gmail.com>
2018-06-19 12:42:10 -0400

    rewrite help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,38 +6,38 @@ module.exports = (bot) => new Command(
     name: 'help',
     description: 'Displays this message, duh!',
     run: async function ({ msg, params, bot }) {
-      let content
-      let command
       if (params[0]) {
-        command = bot.commands.get(params[0]) || bot.commands.get(bot.aliases.get(params[0]))
+        const command = bot.commands.get(params[0]) || bot.commands.get(bot.aliases.get(params[0]))
         if (!command) return `${params[0]} is not a command or alias!`
-        content = command.info
-      } else {
-        content = 'Available commands:```'
-        let commands = []
-        for (let [key, val] of bot.commands) {
-          if (!await bot.permissions.get(val.permission).check(msg.member, bot)) continue
-          commands.push({ name: key, desc: val.description, aliases: val.aliases })
-        }
-        const longName =
-          Math.max(...commands.map((val) => val.name.length + val.aliases.join('/').length)) +
-          3
-        commands = commands.map((val) => {
-          if (val.aliases.length > 0) val.name += '/' + val.aliases.join('/')
-          val.name += ':' + ' '.repeat(longName - val.name.length)
-          return val
-        })
-        for (let i = 0; i < commands.length; i++) {
-          const val = commands[i]
-          content += `\n${val.name}${val.desc}`
-        }
-        content += '\n```\nTo get more information try: `help command`'
+        return sendHelp(msg, command.info)
       }
 
-      msg.author.getDMChannel()
-        .then((dm) => dm.createMessage(content)
-          .then((success) => 'DM sent.'))
-        .catch((e) => content)
+      const permLevel = await bot.permissionLevel(msg.member)
+      let commands = []
+      let longName = 0
+      for (let [key, val] of bot.commands) {
+        if (bot.permissions.get(val.permission).level > permLevel) continue
+        const length = val.name.length + val.aliases.join('/').length
+        if (longName < length + 3) longName = length + 3
+        commands.push({ name: key, desc: val.description, aliases: val.aliases })
+      }
+
+      let content = 'Available commands:```'
+      for (let i = 0; i < commands.length; i++) {
+        const val = commands[i]
+        if (val.aliases.length > 0) val.name += '/' + val.aliases.join('/')
+        val.name += ':' + ' '.repeat(longName - val.name.length)
+        content += `\n${val.name}${val.desc}`
+      }
+      content += '\n```\nTo get more information try: `help command`'
+      return sendHelp(msg, content)
     }
   }
 )
+
+async function sendHelp (msg, content) {
+  return msg.author.getDMChannel()
+    .then((dm) => dm.createMessage(content)
+      .then((success) => 'DM sent.'))
+    .catch((e) => content)
+}
