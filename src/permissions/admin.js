3b33b7a29fc8146cbef5400cddbbbd6ffd330395
Alex Taxiera <alex.taxiera@gmail.com>
2018-06-19 12:41:14 -0400

    store owner ID at ready

--- src/permissions/admin.js
+++ src/permissions/admin.js
@@ -3,5 +3,5 @@ const Permission = require('../classes/Permission.js')
 module.exports = new Permission({
   name: 'Admin',
   level: 100,
-  check: async (member, bot) => member.id === (await bot.getOAuthApplication()).owner.id
+  check: async (member, bot) => member.id === bot.ownerID
 })
