6736367f68883eff068728f0b2eba8c7fc71e37d
Alex Taxiera <alex.taxiera@gmail.com>
2018-06-19 12:41:14 -0400

    store owner ID at ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,5 @@
 module.exports = async (bot) => {
+  bot.ownerID = (await bot.getOAuthApplication()).owner.id
   await bot.dbm.initialize(bot.guilds)
   bot.logger.success('online')
   if (bot.toggles.get('rotateStatus').value) bot.status.startRotate(bot)
