b134bf15dbf834b09df31c318954b00ff0b3a07f
Alex Taxiera <alex.taxiera@gmail.com>
2018-07-06 15:57:16 -0400

    stop setting default setting value in constructor

--- src/settings/status.js
+++ src/settings/status.js
@@ -1,15 +1,12 @@
 const Setting = require('../classes/Setting.js')
 
-module.exports = (bot) => new Setting(
-  bot,
-  {
-    name: 'status',
-    prettyName: 'Default Status',
-    _onChange: (bot, value) => {
-      value = { name: value, type: 0 }
-      bot.dbm.updateDefaultStatus(value)
-      if (bot.toggles.get('rotateStatus').value) return
-      bot.status.setStatus(bot, value)
-    }
+module.exports = new Setting({
+  name: 'status',
+  prettyName: 'Default Status',
+  _onChange: (bot, value) => {
+    value = { name: value, type: 0 }
+    bot.dbm.updateDefaultStatus(value)
+    if (bot.toggles.get('rotateStatus').value) return
+    bot.status.setStatus(bot, value)
   }
-)
+})
--- src/toggles/rotateStatus.js
+++ src/toggles/rotateStatus.js
@@ -1,14 +1,11 @@
 const Toggle = require('../classes/Toggle.js')
 
-module.exports = (bot) => new Toggle(
-  bot,
-  {
-    name: 'rotateStatus',
-    prettyName: 'Rotating Status',
-    _onChange: (bot, value) => {
-      if (value) return bot.status.startRotate()
-      bot.status.endRotate()
-      bot.status.setGame(bot, value)
-    }
+module.exports = new Toggle({
+  name: 'rotateStatus',
+  prettyName: 'Rotating Status',
+  _onChange: (bot, value) => {
+    if (value) return bot.status.startRotate()
+    bot.status.endRotate()
+    bot.status.setGame(bot, value)
   }
-)
+})
