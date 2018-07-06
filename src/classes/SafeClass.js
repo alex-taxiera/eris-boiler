3bfee14e1ae79f2ae68817ab29e82177aca048a0
Alex Taxiera <alex.taxiera@gmail.com>
2018-07-06 16:05:50 -0400

    add some whitespace to align error message with timestamp

--- src/classes/SafeClass.js
+++ src/classes/SafeClass.js
@@ -38,7 +38,7 @@ class SafeClass {
         if (!expected.has(actual)) errors.push(this._restraintError(key, expected, actual))
       }
     }
-    if (errors.length > 0) throw TypeError('\n\t\t' + errors.join('\n\t\t'))
+    if (errors.length > 0) throw TypeError('\n\t\t\u0020' + errors.join('\n\t\t\u0020'))
   }
   /**
    * Build a restraint error message.
