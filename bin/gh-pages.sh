c450bad0b75a4e269ebfc1bdcb534eea4cbd813a
deploy.sh <>
2018-07-11 21:28:09 -0400

    update git info

--- bin/gh-pages.sh
+++ bin/gh-pages.sh
@@ -74,8 +74,8 @@ parse_args() {
 	deploy_branch=${GIT_DEPLOY_BRANCH:-gh-pages}
 
 	#if no user identity is already set in the current git environment, use this:
-	default_username=${GIT_DEPLOY_USERNAME:-deploy.sh}
-	default_email=${GIT_DEPLOY_EMAIL:-}
+	default_username=${GIT_DEPLOY_USERNAME:-alex-taxiera}
+	default_email=${GIT_DEPLOY_EMAIL:-alex.taxiera@gmail.com}
 
 	#repository to deploy to. must be readable and writable.
 	repo=${GIT_DEPLOY_REPO:-origin}
