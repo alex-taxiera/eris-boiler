e488d41157edab7a2b2bb0d84ae2d840522637f8
GitStonic <kingstonicthe1@gmail.com>
2018-07-20 18:03:56 -0400

    fiixxxesss...

--- src/classes/test/Orator.js
+++ src/classes/test/Orator.js
@@ -8,93 +8,93 @@ import Permission from '../Permission'
 import Logger from '../Logger'
 const clientConf = process.env
 
-const client = new Client(clientConf);
+const client = new Client(clientConf)
 
 const permission = new Permission({
-    name: 'Guild Owner',
-    level: 80,
-    check: async member => member === null
+  name: 'Guild Owner',
+  level: 80,
+  check: async member => member === null
 })
 
 client.permissions.set(permission.name, permission)
 
 const command = new Command(client, {
-    name: 'test',
-    description: 'test description',
-    run: () => console.log('hi'),
-    options: {
-        permission: 'Guild Owner'
-    }
+  name: 'test',
+  description: 'test description',
+  run: () => console.log('hi'),
+  options: {
+    permission: 'Guild Owner'
+  }
 })
 
 test.before(t => {
-    t.context.Orator = new Orator(Logger);
+  t.context.Orator = new Orator(Logger)
 })
 
 test.beforeEach(t => {
-    t.context.log = sinon.spy(console, 'log')
-    client.commands.set(command.name, command)
+  t.context.log = sinon.spy(console, 'log')
+  client.commands.set(command.name, command)
 })
 
 test.afterEach(t => {
-    t.context.log.restore()
-    client.permissions.delete(permission.name)
-    client.commands.delete(command.name)
+  t.context.log.restore()
+  client.permissions.delete(permission.name)
+  client.commands.delete(command.name)
 })
 
 test.serial('canExecute', async t => {
-    t.is(await t.context.Orator._canExecute(client, command, ['test'], permission, {
-        member: null
-    }), true)
+  t.is(await t.context.Orator._canExecute(client, command, ['test'], permission, {
+    member: null
+  }), true)
 })
 
 test.serial('getCommand', t => {
-    t.is(t.context.Orator._getCommand(client, 'test'), command)
+  t.is(t.context.Orator._getCommand(client, 'test'), command)
 })
 
 test.serial('Command by user', t => {
-    const msg = {
-        content: '!test',
-        member: {
-            id: 'testing_id'
-        }
+  const msg = {
+    content: '!test',
+    member: {
+      id: 'testing_id'
     }
-    const mockClient = {
-        user: {
-            id: 'other_testing_id'
-        }
+  }
+  const mockClient = {
+    user: {
+      id: 'other_testing_id'
     }
-    t.is(t.context.Orator._isCommandByUser(mockClient, msg, '!'), true)
+  }
+  t.is(t.context.Orator._isCommandByUser(mockClient, msg, '!'), true)
 })
 
 test.serial('Is guild', t => {
-    const msg = {
-        channel: {
-            guild: true
-        }
+  const msg = {
+    channel: {
+      guild: true
     }
-    t.is(t.context.Orator._isGuild(msg), true)
+  }
+  t.is(t.context.Orator._isGuild(msg), true)
 })
 
 test.serial('Parse response', t => {
-    const resp = {
-        content: null,
-        embed: null,
-        file: null
-    };
-    t.deepEqual(t.context.Orator._parseResponse(resp), {
-        content: {
-            content: '',
-            embed: null
-        },
-        file: null
-    })
+  const resp = {
+    content: null,
+    embed: null,
+    file: null
+  }
+  t.deepEqual(t.context.Orator._parseResponse(resp), {
+    content: {
+      content: '',
+      embed: null
+    },
+    file: null
+  })
 })
 
 test.serial('Speed logs', async t => {
-    t.context.Orator._start = 0
-    t.context.Orator._speedLog('test')
-    t.true(t.context.log.calledOnce)
+  t.context.Orator._start = 0
+  t.context.Orator._speedLog('test')
+  t.true(t.context.log.calledOnce)
 })
 
 /* Process Message thingy(That stonic didn't want to do)...
