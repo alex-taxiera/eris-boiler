71355da995b62579a6913f7a4c2d3f2ef886a0ba
GitStonic <kingstonicthe1@gmail.com>
2018-07-19 22:18:41 -0400

    testing with this db setup on travis(I have no idea what i'm doing)...

--- src/classes/DataClient.js
+++ src/classes/DataClient.js
@@ -172,7 +172,7 @@ class DataClient extends require('eris').Client {
   }
 
   _inCache (id, cache) {
-    return this[cache].get(id) != null
+    return this[cache].get(id) !== null
   }
   /**
    * Load data files.
