a11a64a3de6f16c56635daa52f1d80f7bf41fe6f
GitStonic <kingstonicthe1@gmail.com>
2018-08-06 20:11:26 -0400

    gotta fix this hoe

--- src/classes/test/Permission.js
+++ src/classes/test/Permission.js
@@ -1,15 +1,15 @@
-import test from 'ava';
+import test from 'ava'
 
-import Permission from '../Permission';
+import Permission from '../Permission'
 const mockPermData = {
-    name: 'test-perm',
-    level: 0
+  name: 'test-perm',
+  level: 0
 }
 
 test.before((t) => {
-    t.context.Permission = new Permission(mockPermData)
+  t.context.Permission = new Permission(mockPermData)
 })
 
 test('Deny permission', (t) => {
-    t.is(t.context.Permission.deny, 'Must be test-perm!')
+  t.is(t.context.Permission.deny, 'Must be test-perm!')
 })
