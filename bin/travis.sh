86e24bd845c516959c6a849f4d0cf91d3d54f6bb
alex-taxiera <alex.taxiera@gmail.com>
2018-08-31 13:15:26 -0400

    test out a custom basj script

---
+++ bin/travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == 'master' ]]
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+  chmod +x ./cc-test-reporter
+  ./cc-test-reporter before-build
+fi
+npm test
+if [[ $TRAVIS_BRANCH == 'master' ]]
+  ./cc-test-reporter after-build --exit-code $TRAVIS_TEST_RESULT
+fi
