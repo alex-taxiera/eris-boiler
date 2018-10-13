af6e22643f643b2c1eecf9e9f1195ee9269e0212
alex-taxiera <alex.taxiera@gmail.com>
2018-10-12 20:12:56 -0400

    update demo

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,7 @@
 require('dotenv').load()
 const { DataClient } = require('../')
+const token = process.env.TOKEN
 
-const bot = new DataClient({sourceFolder: './demo'})
+const bot = new DataClient({ token, sourceFolder: './demo' })
 /* log bot into Discord */
 bot.connect()
