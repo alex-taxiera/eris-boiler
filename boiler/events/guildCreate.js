a6470903d1a013f167e2e93f3d90a6dfa1c373e4
Alex Taxiera <alex.taxiera@gmail.com>
2019-05-07 10:59:27 -0400

    clean up file loading a bit and bake in some event handling

--- boiler/events/guildCreate.js
+++
@@ -1,9 +0,0 @@
-const { Event, Utils } = require('../../lib')
-
-module.exports = new Event({
-  name: 'guildCreate',
-  run: (bot, guild) => {
-    Utils.logger.success(`joined ${guild.name} guild`)
-    bot.dbm.newObject('guild').save({ id: guild.id })
-  }
-})
