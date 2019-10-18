2b13554bbc8e1295779ebd62819f9d3e70a1d275
GitStonic <kingstonicthe1@gmail.com>
2019-10-17 22:29:45 -0400

    this really don't like me...

--- cli/index.js
+++ cli/index.js
@@ -1,6 +1,34 @@
 // const arg = require('arg')
 const createProject = require('./main.js')
 // const inquirer = require('inquirer')
+const fs = require('fs')
+const path = require('path')
+const {
+  promisify
+} = require('util')
+
+// const access = promisify(fs.access)
+const copy = promisify(fs.copyFile)
+
+const copyFiles = () => {
+  return copy(path.join(__dirname, 'template'), process.cwd(), {
+    clobber: false
+  })
+}
+
+module.exports = async () => {
+  process.stdout.write('Copying files...\n')
+
+  try {
+    await copyFiles()
+  } catch (error) {
+    process.stderr.write(`Failed to copy files:\n${error}\n`)
+    return false
+  }
+
+  process.stdout.write('Copied template files...\n')
+  return true
+}
 
 // const parseArgs = (rawArgs) => {
 //   const args = arg(
