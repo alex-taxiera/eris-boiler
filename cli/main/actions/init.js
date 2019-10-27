518edb0d1ffff5603d171de191be643c027be159
GitStonic <kingstonicthe1@gmail.com>
2019-10-27 19:31:05 -0400

    おはいよう

--- cli/main/actions/init.js
+++ cli/main/actions/init.js
@@ -11,20 +11,20 @@ class Initialize extends Action {
     super({
       name: 'init',
       async run (params) {
-        print('Importing the template...\n')
+        print('Importing the template...')
 
         const templateDir = resolve(__dirname, '..', 'templates')
         const userDir = resolve(process.cwd())
 
         copy(templateDir, userDir)
 
-        print('Loading package.json\n')
+        print('Loading package.json')
         exec('npm init -y', (error, stdout, stderr) => {
           if (error) {
             return print(error.toString())
           } else {
             print(stdout)
-            print('Installing dependencies\n')
+            print('Installing dependencies')
             exec('npm install eris eris-boiler', (error, stdout, stderr) => {
               if (error) {
                 return print(error.toString())
--- cli/main/functions/index.js
+++ cli/main/functions/index.js
@@ -1,5 +1,22 @@
+const fs = require('fs').promises
+
 module.exports.print = (...input) => {
-  process.stdout.write(input.join(' '))
+  process.stdout.write(input.join(' ') + '\n')
+}
+
+module.exports.copy = async (parent, target) => {
+  const files = await fs.readdir(parent)
+
+  for (const file of files) {
+    try {
+      await fs.copyFile(`${parent}/${file}`, `${target}/${file}`)
+      this.print('Copied files...')
+    } catch (error) {
+      this.print(
+        `An error occurred running migrations, reason:\n${error}`
+      )
+    }
+  }
 }
 
 module.exports.parseArgs = (rawArgs) => {
