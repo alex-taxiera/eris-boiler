bf87206c41c7e23de0ad02009608bfef7556ceab
Alex Taxiera <alex.taxiera@onesky.com>
2020-01-21 16:08:29 -0500

    declare logger module

---
+++ util/logger/index.d.ts
@@ -0,0 +1,6 @@
+declare module 'eris-boiler/utils/logger' {
+  declare function success (...args: any[]): unknown
+  declare function warn (...args: any[]): unknown
+  declare function error (...args: any[]): unknown
+  declare function info (...args: any[]): unknown
+}
