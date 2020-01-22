3deeb3b572274f4840eef8a5f61b44de09503eba
Alex Taxiera <alex.taxiera@gmail.com>
2020-01-21 19:49:36 -0500

    try combining module files

--- index.d.ts
+++ index.d.ts
@@ -293,3 +293,30 @@ declare module 'eris-boiler' {
   }
 }
 
+declare module 'eris-boiler/util' {
+  type Key = string | number
+  
+  type FilterCallback<T> = (item: T) => boolean
+  
+  class ExtendedMap<Key, T> extends Map<Key,T> {
+    find(func: FilterCallback<T>): T | void
+    filter(func: FilterCallback<T>): T[]
+    map<R>(func: (item: T) => R): R[]
+    reduce(func: (accumulator: T, item: T) => T, initialValue?: T): T
+    reduce<R>(func: (accumulator: R, item: T) => R, initialValue: R): R
+    every(func: FilterCallback<T>): boolean
+    some(func: FilterCallback<T>): boolean
+  }
+  
+  type Status = {
+    name: string
+    type: number
+  }
+}
+
+declare module 'eris-boiler/util/logger' {
+  function success (...args: any[]): unknown
+  function warn (...args: any[]): unknown
+  function error (...args: any[]): unknown
+  function info (...args: any[]): unknown
+}
--- util/logger/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'eris-boiler/util/logger' {
-  function success (...args: any[]): unknown
-  function warn (...args: any[]): unknown
-  function error (...args: any[]): unknown
-  function info (...args: any[]): unknown
-}
