0c6bc9ffd3818e3186f92d28201fc31020cbd421
Alex Taxiera <alex.taxiera@onesky.com>
2020-03-04 18:24:32 -0500

    add dm safeguards for permissions

--- permissions/owner.js
+++ permissions/owner.js
@@ -2,5 +2,11 @@ const { Permission } = require('../lib')
 
 module.exports = new Permission({
   level: 100,
-  run: async (bot, { msg: { member } }) => member.id === member.guild.ownerID
+  run: async (bot, { msg: { member } }) => {
+    if (!member) {
+      return false
+    }
+
+    return member.id === member.guild.ownerID
+  }
 })
--- permissions/vip.js
+++ permissions/vip.js
@@ -3,6 +3,10 @@ const { Permission } = require('../lib')
 module.exports = new Permission({
   level: 50,
   run: async (bot, { msg: { member } }) => {
+    if (!member) {
+      return false
+    }
+
     const dbGuild = await bot.dbm.newQuery('guild').get(member.guild.id)
     const { vip } = dbGuild.toJSON()
     return vip && member.roles.includes(vip)
