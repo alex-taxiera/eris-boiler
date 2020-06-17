444efa4eb8d422ed50a174e5bb1f9de53c672c42
Alex Taxiera <alex.taxiera@gmail.com>
2020-06-16 20:22:57 -0400

    add message utils

---
+++ packages/@eris-boiler/common/src/utils/message.ts
@@ -0,0 +1,31 @@
+import {
+  Message,
+  PrivateChannel,
+  GroupChannel,
+  TextChannel,
+  NewsChannel
+} from 'eris'
+
+export function isInDM (
+  message: Message
+): message is Message<PrivateChannel> {
+  return message.channel.type === 0 || message.channel.type === 3
+}
+
+export function isInGroupDM (
+  message: Message
+): message is Message<GroupChannel> {
+  return message.channel.type === 3
+}
+
+export function isInGuild (
+  message: Message
+): message is Message<TextChannel> {
+  return message.channel.type === 1 || message.channel.type === 5
+}
+
+export function isInNews (
+  message: Message
+): message is Message<NewsChannel> {
+  return message.channel.type === 5
+}
