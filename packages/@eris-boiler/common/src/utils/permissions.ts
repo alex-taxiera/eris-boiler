ad18afcafde47e3c2e0969f14e34658a70a17f64
Alex Taxiera <alex.taxiera@gmail.com>
2020-06-18 23:39:55 -0400

    add permission helpers

---
+++ packages/@eris-boiler/common/src/utils/permissions.ts
@@ -0,0 +1,40 @@
+import {
+  TextChannel,
+  Constants,
+  Message,
+} from 'eris'
+import { isInGuild } from './message'
+
+export type PermissionKeys = Array<keyof Constants['Permissions']>
+
+export const requiredSendPermissions: PermissionKeys = [
+  'readMessages',
+  'sendMessages',
+]
+export const requiredDeletePermissions: PermissionKeys = [
+  'manageMessages',
+]
+
+export function checkChannelPermissions (
+  memberId: string,
+  channel: TextChannel,
+  permissions: PermissionKeys,
+): boolean {
+  const permission = channel.permissionsOf(memberId)
+  return permissions.every((perm) => permission.has(perm))
+}
+
+export function canSend (memberId: string, channel: TextChannel): boolean {
+  return checkChannelPermissions(memberId, channel, requiredSendPermissions)
+}
+
+export function canDelete (memberId: string, message: Message): boolean {
+  return memberId === message.author.id || (
+    isInGuild(message) &&
+    checkChannelPermissions(
+      memberId,
+      message.channel,
+      requiredDeletePermissions,
+    )
+  )
+}
