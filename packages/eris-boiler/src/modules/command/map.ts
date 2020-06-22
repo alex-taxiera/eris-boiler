060497a853d651331f0195e607ed84aa3071f8e2
Alex Taxiera <alex.taxiera@gmail.com>
2020-06-21 22:28:14 -0400

    add reloader

--- packages/eris-boiler/src/modules/command/map.ts
+++ packages/eris-boiler/src/modules/command/map.ts
@@ -19,6 +19,18 @@ export class CommandMap<C extends Client> extends LoadMap<Command<C>> {
     this.set(loadableObject.name, loadableObject)
   }
 
+  protected _reload (oldLoadableObject: any, loadableObject: any): void {
+    if (!(
+      loadableObject instanceof Command && oldLoadableObject instanceof Command
+    )) {
+      throw TypeError('Invalid Command found during reloading')
+    }
+    if (oldLoadableObject.name !== loadableObject.name) {
+      this.delete(oldLoadableObject.name)
+    }
+    return this._load(loadableObject)
+  }
+
   public search (key: string): Command<C> | undefined {
     return this.find(
       (command) => command.name.toLowerCase() === key.toLowerCase() ||
